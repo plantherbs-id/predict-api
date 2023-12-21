479975e6bd336a3288e24d0bd2a69c0125c465d1
kharistya281 <kharistya281@gmail.com>
2023-12-21 17:36:36 +0700

    Edit main.py

--- main.py
+++ main.py
@@ -208,5 +208,5 @@ def index():
     return 'OK'
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
